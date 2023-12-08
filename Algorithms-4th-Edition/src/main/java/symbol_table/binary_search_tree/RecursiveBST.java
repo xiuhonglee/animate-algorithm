1ce8655e39fc90616afad41e504b55d4c19c7b32
xiuhong <hz_likun@zju.edu.cn>
2023-12-08 15:53:44 +0800

    add: comment for BST

--- Algorithms-4th-Edition/src/main/java/symbol_table/binary_search_tree/RecursiveBST.java
+++ Algorithms-4th-Edition/src/main/java/symbol_table/binary_search_tree/RecursiveBST.java
@@ -24,6 +24,7 @@ public class RecursiveBST<Key extends Comparable<Key>, Value> implements Ordered
 
     /**
      * 外部公开接口-调用内部方法
+     *
      * @param key
      * @param val
      */
@@ -39,21 +40,30 @@ public class RecursiveBST<Key extends Comparable<Key>, Value> implements Ordered
 
     /**
      * 插入操作具体实现-递归
+     *
      * @param node
      * @param key
      * @param val
      * @return
      */
     private Node put(Node node, Key key, Value val) {
+        // 如果当前节点为空，说明找到了插入位置，创建新节点并返回
         if (node == null) return new Node(key, val, 1);
+
+        // 比较插入键与当前节点键的大小
         int cmp = key.compareTo(node.key);
 
+        // 如果插入键小于当前节点键，则在左子树中继续插入
         if (cmp < 0) node.left = put(node.left, key, val);
+            // 如果插入键大于当前节点键，则在右子树中继续插入
         else if (cmp > 0) node.right = put(node.right, key, val);
+            // 如果插入键等于当前节点键，则替换该节点的值
         else node.val = val;
 
+        // 更新当前节点的子树大小
         node.size = 1 + size(node.left) + size(node.right);
 
+        // 返回当前节点，以便将结构变化反映到整个树中
         return node;
     }
 
@@ -65,16 +75,25 @@ public class RecursiveBST<Key extends Comparable<Key>, Value> implements Ordered
 
     /**
      * 查找-递归实现
+     *
      * @param node
      * @param key
      * @return
      */
     private Value get(Node node, Key key) {
+        // 如果节点为空，说明键不存在于树中，返回null
         if (node == null) return null;
+
+        // 比较查找键与当前节点键的大小
         int cmp = key.compareTo(node.key);
 
+        // 如果查找键小于当前节点键，继续在左子树中查找
         if (cmp < 0) return get(node.left, key);
+
+        // 如果查找键大于当前节点键，在右子树中继续查找
         else if (cmp > 0) return get(node.right, key);
+
+        // 如果查找键等于当前节点键，返回该节点的值
         else return node.val;
     }
 
@@ -85,19 +104,37 @@ public class RecursiveBST<Key extends Comparable<Key>, Value> implements Ordered
     }
 
     private Node delete(Node node, Key key) {
+        // 如果节点为空，表示键不存在于树中，返回null
         if (node == null) return null;
 
+        // 比较删除键与当前节点键的大小
         int cmp = key.compareTo(node.key);
+        // 如果删除键小于当前节点键，在左子树中继续删除
         if (cmp < 0) node.left = delete(node.left, key);
+            // 如果删除键大于当前节点键，在右子树中继续删除
         else if (cmp > 0) node.right = delete(node.right, key);
+            // 找到了要删除的节点
         else {
+            // 如果该节点只有右子树或无子树，直接返回右子节点
             if (node.left == null) return node.right;
+            // 如果该节点只有左子树，直接返回左子节点
             if (node.right == null) return node.left;
+
+            // 如果该节点既有左子树又有右子树
             Node t = node;
+
+            /**
+             * 下面一般有两种处理方法(我们用的是 第1种 方法)：
+             * 方法 1（常用）：找到右子树中的最小节点，用它来替换当前节点
+             * 方法 2：你也可以找到左子树最大的结点来替换被删除的节点
+             */
             node = min(t.right);
+            // 删除右子树中的最小节点
             node.right = deleteMin(t.right);
+            // 保持左子树不变
             node.left = t.left;
         }
+        // 更新节点的大小
         node.size = size(node.left) + size(node.right) + 1;
         return node;
     }
