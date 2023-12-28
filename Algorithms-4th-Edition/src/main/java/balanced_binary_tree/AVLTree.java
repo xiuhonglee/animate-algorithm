7f3e3609f9e888b923c97e01da51d869e65e1c02
xiuhong <hz_likun@zju.edu.cn>
2023-12-28 13:20:36 +0800

    rewrite AVL tree

--- Algorithms-4th-Edition/src/main/java/balanced_binary_tree/AVLTree.java
+++ Algorithms-4th-Edition/src/main/java/balanced_binary_tree/AVLTree.java
@@ -1,208 +1,179 @@
 package balanced_binary_tree;
 
-public class AVLTree {
+class AVLTree {
 
-    // AVL树的节点类定义
     public class Node {
-        int key;        // 节点的键
-        int height;     // 节点的高度
-        int balanceFactor; // 节点的平衡因子
-        Node left;      // 指向左子节点
-        Node right;     // 指向右子节点
+        int key, height;
+        Node left, right;
 
-        Node(int key) {
-            this.key = key;
-            this.height = 0;
-            this.balanceFactor = 0;
+        Node(int d) {
+            key = d;
+            height = 1;
         }
     }
+    private Node root;
 
-    private Node root; // AVL树的根节点
+    // 一个工具函数，用于获取树的高度
+    int height(Node N) {
+        if (N == null)
+            return 0;
 
-    // 查找方法：按键查找节点
-    public Node get(int key) {
-        Node current = root;
-        while (current != null) {
-            if (current.key == key) {
-                break;
-            }
-            current = current.key < key ? current.right : current.left;
-        }
-        return current;
+        return N.height;
     }
 
-    // 插入方法：在AVL树中插入新的键
-    public void put(int key) {
-        root = put(root, key);
+    // 一个工具函数，用于获取两个整数的最大值
+    int max(int a, int b) {
+        return (a > b) ? a : b;
     }
 
-    // 删除方法：从AVL树中删除键
-    public void delete(int key) {
-        root = delete(root, key);
+    // 一个工具函数，用于对以y为根的子树进行右旋
+    // 参见上文给出的图示。
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        // 执行旋转
+        x.right = y;
+        y.left = T2;
+
+        // 更新高度
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+
+        // 返回新的根节点
+        return x;
     }
 
-    // 获取根节点
-    public Node getRoot() {
-        return root;
+    // 一个工具函数，用于对以x为根的子树进行左旋
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        // 执行旋转
+        y.left = x;
+        x.right = T2;
+
+        // 更新高度
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+
+        // 返回新的根节点
+        return y;
     }
 
-    // 获取树的高度
-    public int height() {
-        return root == null ? -1 : root.height;
+    // 获取节点N的平衡因子
+    int getBalance(Node N) {
+        if (N == null)
+            return 0;
+
+        return height(N.left) - height(N.right);
     }
 
-    // 私有插入方法：在指定的节点下插入新键
-    private Node put(Node root, int key) {
-        if (root == null) {
-            return new Node(key);
-        } else if (root.key > key) {
-            root.left = put(root.left, key);
-        } else if (root.key < key) {
-            root.right = put(root.right, key);
-        } else {
-            throw new RuntimeException("duplicate Key!");
-        }
-        return rebalance(root);
-    }
+    Node insert(Node node, int key) {
 
-    // 更新节点的高度和平衡因子
-    private void updateHeightAndBalance(Node n) {
-        int leftHeight = height(n.left);
-        int rightHeight = height(n.right);
-        n.height = 1 + Math.max(leftHeight, rightHeight);
-        n.balanceFactor = rightHeight - leftHeight;
-    }
+        /* 1. 执行正常的BST插入 */
+        if (node == null)
+            return (new Node(key));
 
-    // 私有删除方法：从指定节点下删除键
-    private Node delete(Node node, int key) {
-        if (node == null) {
+        if (key < node.key)
+            node.left = insert(node.left, key);
+        else if (key > node.key)
+            node.right = insert(node.right, key);
+        else // 不允许重复键
             return node;
-        } else if (node.key > key) {
-            node.left = delete(node.left, key);
-        } else if (node.key < key) {
-            node.right = delete(node.right, key);
-        } else {
-            if (node.left == null || node.right == null) {
-                node = (node.left == null) ? node.right : node.left;
-            } else {
-                Node mostLeftChild = mostLeftChild(node.right);
-                node.key = mostLeftChild.key;
-                node.right = delete(node.right, node.key);
-            }
+
+        /* 2. 更新这个祖先节点的高度 */
+        node.height = 1 + max(height(node.left),
+                height(node.right));
+
+        /* 3. 获取这个祖先节点的平衡因子
+            以检查这个节点是否变得不平衡 */
+        int balance = getBalance(node);
+
+        /** 情况1: 左左情况 (T1, T2, T3和T4是子树)
+         *        z                                       y
+         *       / \                                    /   \
+         *      y   T4          右旋 (z)               x      z
+         *     / \          - - - - - - - - ->      /  \    /  \
+         *    x   T3                               T1  T2  T3  T4
+         *   / \
+         * T1   T2
+         */
+        if (balance > 1 && key < node.left.key)
+            return rightRotate(node);
+
+        /** 情况2: 右右情况
+         *    z                                y
+         *   /  \                            /   \
+         *  T1   y     Left Rotate(z)       z      x
+         *      /  \   - - - - - - - ->    / \    / \
+         *     T2   x                     T1  T2 T3  T4
+         *         / \
+         *       T3  T4
+         */
+        if (balance < -1 && key > node.right.key)
+            return leftRotate(node);
+
+        /** 情况3: 左右情况
+         *       z                               z                           x
+         *      / \                            /   \                        /  \
+         *     y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
+         *    / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+         *  T1   x                          y    T3                    T1  T2 T3  T4
+         *      / \                        / \
+         *    T2   T3                    T1   T2
+         */
+        if (balance > 1 && key > node.left.key) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
         }
-        if (node != null) {
-            node = rebalance(node);
+
+        /** 情况4: 右左情况
+         *    z                            z                            x
+         *   / \                          / \                          /  \
+         * T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
+         *     / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+         *    x   T4                      T2   y                  T1  T2  T3  T4
+         *   / \                              /  \
+         * T2   T3                           T3   T4
+         */
+        if (balance < -1 && key < node.right.key) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
+
+        /* 返回（未改变的）节点指针 */
         return node;
     }
 
-    // 找到最左侧的子节点
-    private Node mostLeftChild(Node node) {
-        Node current = node;
-        // 循环找到最左侧的叶子节点
-        while (current.left != null) {
-            current = current.left;
+    // 一个工具函数，用于打印树的前序遍历
+    // 该函数还打印每个节点的高度
+    void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.key + " ");
+            preOrder(node.left);
+            preOrder(node.right);
         }
-        return current;
-    }
-
-    // 重新平衡节点
-    private Node rebalance(Node z) {
-        // 更新节点z的高度和平衡因子
-        updateHeightAndBalance(z);
-        // 获取z的平衡因子
-        int balance = z.balanceFactor;
-
-        // 检测并修正RR（右右）或RL（右左）不平衡的情况
-        if (balance > 1) {
-            // RR情况：z的右子树比左子树高，且z的右子节点的右子树比左子树高
-            if (height(z.right.right) > height(z.right.left)) {
-                z = rotateLeft(z); // 进行左旋转
-            } else {
-                // RL情况：z的右子树比左子树高，但z的右子节点的左子树比右子树高
-                z.right = rotateRight(z.right); // 先对z的右子节点进行右旋转
-                z = rotateLeft(z); // 然后对z进行左旋转
-            }
-        }
-        // 检测并修正LL（左左）或LR（左右）不平衡的情况
-        else if (balance < -1) {
-            // LL情况：z的左子树比右子树高，且z的左子节点的左子树比右子树高
-            if (height(z.left.left) > height(z.left.right)) {
-                z = rotateRight(z); // 进行右旋转
-            } else {
-                // LR情况：z的左子树比右子树高，但z的左子节点的右子树比左子树高
-                z.left = rotateLeft(z.left); // 先对z的左子节点进行左旋转
-                z = rotateRight(z); // 然后对z进行右旋转
-            }
-        }
-
-        // 返回经过旋转调整后的新根节点
-        return z;
-    }
-
-
-    // 右旋转
-    private Node rotateRight(Node y) {
-        Node x = y.left;
-        Node z = x.right;
-        x.right = y;
-        y.left = z;
-
-        updateHeightAndBalance(y);
-        updateHeightAndBalance(x);
-        return x;
-    }
-
-    // 左旋转
-    private Node rotateLeft(Node y) {
-        Node x = y.right;
-        Node z = x.left;
-        x.left = y;
-        y.right = z;
-
-        updateHeightAndBalance(y);
-        updateHeightAndBalance(x);
-        return x;
-    }
-
-    // 更新节点的高度
-    private void updateHeight(Node n) {
-        n.height = 1 + Math.max(height(n.left), height(n.right));
-
-    }
-
-    // 获取指定节点的高度
-    private int height(Node n) {
-        return n == null ? -1 : n.height;
-    }
-
-    // 获取节点的平衡因子
-    public int getBalance(Node n) {
-        return (n == null) ? 0 : height(n.right) - height(n.left);
     }
 
     public static void main(String[] args) {
-        AVLTree avlTree = new AVLTree();
+        AVLTree tree = new AVLTree();
 
-        // 测试插入
-        avlTree.put(10);
-        avlTree.put(20);
-        avlTree.put(30);
-        avlTree.put(40);
-        avlTree.put(50);
-        avlTree.put(25);
+        tree.root = tree.insert(tree.root, 10);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 25); 
 
-        // 测试查找
-        System.out.println("查找键为30的节点: " + avlTree.get(30).key);
-
-        // 输出树的根节点和高度
-        System.out.println("树的根节点: " + avlTree.getRoot().key);
-        System.out.println("树的高度: " + avlTree.height());
-
-        // 测试删除
-        avlTree.delete(20);
-        System.out.println("删除键为20的节点后的树的根节点: " + avlTree.getRoot().key);
-        System.out.println("删除键为20的节点后的树的高度: " + avlTree.height());
+        /** 构造出的AVL树如下
+                30
+                / \
+               20  40
+              / \	 \
+             10 25   50
+        */
+        System.out.println("构造的树的前序遍历为：");
+        tree.preOrder(tree.root);
     }
 }
-
